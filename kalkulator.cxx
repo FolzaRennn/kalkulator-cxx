901a5e9c988336524fcf84554e8f9cadeaa1e6e8
FolzaRennn <146518013+FolzaRennn@users.noreply.github.com>
2024-03-06 11:18:32 +0700

    Update kalkulator.cxx

--- kalkulator.cxx
+++ kalkulator.cxx
@@ -5,10 +5,10 @@
 int main() {
     float angka1, angka2;
     
-    std::cout << "Masukkan angka pertama: ";
+    std::cout << " > Masukkan angka pertama: ";
     std::cin >> angka1;
     
-    std::cout << "Masukkan angka kedua: ";
+    std::cout << " > Masukkan angka kedua: ";
     std::cin >> angka2;
 
     float tambah = angka1 + angka2;
@@ -16,11 +16,18 @@ int main() {
     float kali = angka1 * angka2;
     
     float bagi = (angka2 != 0) ? (angka1 / angka2) : 0;
-
+    // Jika dibagi dengan angka 0 maka hasilnya akan tetap 0
+    
+    std::cout << "==============================" << std::endl;
+    std::cout << "" << std::endl;
     std::cout << "Hasil penambahan: " << tambah << std::endl;
     std::cout << "Hasil pengurangan: " << kurang << std::endl;
     std::cout << "Hasil perkalian: " << kali << std::endl;
     std::cout << "Hasil pembagian: " << bagi << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Program by @ptuurrhmn" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "==============================" << std::endl;
 
     return 0;
 }
